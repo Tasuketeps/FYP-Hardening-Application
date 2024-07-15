0fd1051a65dd3c8a4d3594e3233e76b2670cee88
whoisnaeem <whoisnaeem@gmail.com>
2024-07-15 20:23:02 +0800

    changes by naeem

--- Enforcing_and_Scanning_Team/Naeem/Web_App/backend/server.js
+++ Enforcing_and_Scanning_Team/Naeem/Web_App/backend/server.js
@@ -1,15 +1,18 @@
 // Name: Mohammad Naeem bin Mohammad Rafi
 // Admission number: P2205625
 // Class: DISM/FT/2A/04
-const os = require('os');
-var express = require('express');
-var serveStatic = require('serve-static');
-var app = require('./controller/app.js');
+var app = require('./controller/app')
+var fs = require('fs');
+var https = require('https');
+var path = require('path');
+var hostname="192.168.126.148";
+var port=3000;
 
-var port = 3000;
+const options = {
+        key: fs.readFileSync(path.join(__dirname,'/cert/key.pem')),
+        cert: fs.readFileSync(path.join(__dirname,'/cert/cert.pem'))
+}
 
-app.use(serveStatic(__dirname + '/public')); 
 
-var server = app.listen(port, function(){
-    console.log('Backend server hosted at http://localhost:%s', port);
-});
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(port);
--- Enforcing_and_Scanning_Team/Naeem/Web_App/frontend/server.js
+++ Enforcing_and_Scanning_Team/Naeem/Web_App/frontend/server.js
@@ -1,20 +1,26 @@
-const express=require('express');
-
-var hostname="localhost";
+var express=require('express');
+var fs = require('fs');
+var https = require('https');
+var path = require('path');
+var hostname="192.168.126.148";
 var port=3001;
 
 var app=express();
 
+const options = {
+        key: fs.readFileSync(path.join(__dirname,'/cert/key.pem')),
+        cert: fs.readFileSync(path.join(__dirname,'/cert/cert.pem'))
+}
+
 
 app.use(function(req,res,next){
     const {method , url} = req;
-    console.log(`Received Method = ${method} Url = ${url}`);
-    next();
+    console.log(`Received Method = ${method} Url = ${url}`);    next();
 });
+
 
 app.use(express.static(__dirname+"/public"));
 
 
-app.listen(port,hostname,function(){
-    console.log(`Frontend Server hosted at http://${hostname}:${port}`);
-});
+const httpsServer = https.createServer(options, app);
+httpsServer.listen(port);
